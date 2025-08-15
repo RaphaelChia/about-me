41f63c3a889e7fda0971b6b636f8cf6017b389f5
RaphaelChia <clone_zone@hotmail.com>
2025-08-15 16:47:43 +0800

    link fix

--- src/components/general/link-button.tsx
+++ src/components/general/link-button.tsx
@@ -39,7 +39,7 @@ const LinkButton = ({
           children
         )}
       </Link>
-      &nbsp;
+
       {external ? (
         <IconExternalLink
           className={cn("inline", hideIconMobile && "max-md:hidden")}
