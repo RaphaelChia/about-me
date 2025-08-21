644879212adaadfad00cba56e849d4ef6ce586e8
RaphaelChia <clone_zone@hotmail.com>
2025-08-22 00:25:45 +0800

    lab page redirect

--- src/app/lab/page.tsx
+++ src/app/lab/page.tsx
@@ -1,5 +1,8 @@
+import { redirect } from "next/navigation";
+
 const Page = () => {
-  return <div className="bg-blue-200 grow pad-x-page">abcd page</div>;
+  redirect("/");
+  //   return <div className="bg-blue-200 grow pad-x-page">abcd page</div>;
 };
 
 export default Page;
