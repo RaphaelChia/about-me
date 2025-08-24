dd7a30c14539266acf7aa3ebd57ab853b21107dc
RaphaelChia <clone_zone@hotmail.com>
2025-08-24 14:28:16 +0800

    fontsize change for checks description

--- src/app/lab/checks/layout.tsx
+++ src/app/lab/checks/layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <span className="font-mono text-3xl font-bold">
           virtualization, compression, checkboxes.
         </span>
-        <p className="font-mono text-sm text-foreground-neutral">
+        <p className="font-mono text-sm text-foreground-neutral max-md:text-xs">
           There&apos;s a 100,000 checkboxes. The goal is to simulate large
           number of items, manage their states, and come up with a more
           efficient storing technique to save the large number of states.
